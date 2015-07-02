eb95d8265d17bb2473133bb44e91a49d0bb8bb9a
Peter Andrews <peter.andrews@modolabs.com>
2015-07-02 14:36:21 -0400

    Formatting fixes

--- rolldembones.py
+++ rolldembones.py
@@ -25,17 +25,18 @@ def main():
             rolls += 1
 
         print("Target:", args.target)
-        print("Rolls:", rolls)
-        print("Outcome:", tally)
+        print(" Rolls:", rolls)
+        print(" Tally:", tally)
 
 if __name__ == '__main__':
+    version = "0.5"
     parser = argparse.ArgumentParser(prog="Roll Dem Bones", description="Roll some dice.")
 
     parser.add_argument("-r", "--repeat", dest="repeats", metavar="N", type=int, default=None, help="Repeat these rolls N times. When used alongside -u, a maximum of N rolls will be made.")
     parser.add_argument("-u", "--repeat-until", dest="target", metavar="T", type=int, default=None, help="Repeat these rolls as many times as needed until all their tallies combined match or exceed T. Overrides -m.")
     parser.add_argument("-e", "--reroll", dest="explode", metavar="T", type=int, default=None, help="Any die whose roll matches or exceeds T is counted and rolled again. Setting T to 1 or lower disables re-rolls.")
     parser.add_argument("-m", "--mode", dest="mode", type=str, default=None, choices=['spread', 'tally'], help="Force all rolls to use the given MODE instead of each roll's default. Ignored when using -u.")
-    parser.add_argument("--version", action="version", version="%(prog)s 2.0")
+    parser.add_argument("--version", action="version", version="%(prog)s v{0}".format(version))
     parser.add_argument("dice", nargs='*', help="Dice to roll, given in pairs of the number of dice to roll, and the sides those dice have.")
 
     group_nwod = parser.add_argument_group('New World of Darkness', 'Options with special behavior for the nwod die type.')
