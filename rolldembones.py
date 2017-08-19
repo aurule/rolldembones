4225c022a8c9dcab6aebe3a9467713293686b5f7
Peter Andrews <pmandrews@gmail.com>
2017-08-19 12:42:55 -0400

    Move version to top level

--- rolldembones.py
+++ rolldembones.py
@@ -86,8 +86,9 @@ def main(args):
         logger.error("Unknown die type '{0}'".format(e))
         return 1
 
+__version__ = "1.0b1"
+
 if __name__ == '__main__':
-    version = "1.0b1"
     parser = argparse.ArgumentParser(prog="Roll Dem Bones", description="Roll some dice.")
 
     parser.add_argument("-r", "--repeat", dest="repeats", metavar="N", type=int, default=None, help="Repeat the described rolls %(metavar)s times.")
@@ -96,7 +97,7 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--roll-again", dest="explode", metavar="T", type=int, default=None, help="Any time a die's roll matches or exceeds %(metavar)s, it is counted and rolled again.")
     parser.add_argument("-s", "--sum", dest="aggregate", action="store_true", default=False, help="Display the sum of all dice in the roll. In spread mode, counts the rolled faces for each die. In tally mode, counts the number of dice which exceeded the threshold.")
     parser.add_argument("--debug", dest="debug", action="store_true", default=False, help="Show the raw die data for each repetition")
-    parser.add_argument("--version", action="version", version="%(prog)s v{0}".format(version))
+    parser.add_argument("--version", action="version", version="%(prog)s v{0}".format(__version__))
     parser.add_argument("dice", nargs='*', help="Dice to roll, given as the number of dice followed by the number of sides those dice have. Supports dice with consecutive and increasing numerical sides, as well as two special types of die: nwod and fate.")
 
     group_nwod = parser.add_argument_group('New World of Darkness', 'Options with special behavior for the nwod die type.')
